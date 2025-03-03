527c7122b4004e40d52d38779941117f30c521ae
Alejandro <alejobetancur2@gmail.com>
2025-03-03 12:05:13 -0500

    PORT correction

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const PORT = 3007;
+const PORT = 3021;
 const allowedOrigins = ['http://localhost:5173', 'https://ultim-admin-dashboard.web.app', 'https://ultim-admin-dashboard.firebaseapp.com', 'https://dashboard.ultim.pro', 'https://ultim.pro', 'https://trasnferchat-1336.twil.io/transferChat', 'https://trasnferchat-1336.twil.io', 'https://ultim.pro/dashboard/carestream/chat-carestream'];
 app.use(cors({
     origin: function (origin, callback) {
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 app.options('*', cors()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
-const PORT = 3007;
+const PORT = 3021;
 
 const allowedOrigins = ['http://localhost:5173', 'https://ultim-admin-dashboard.web.app', 'https://ultim-admin-dashboard.firebaseapp.com', 'https://dashboard.ultim.pro', 'https://ultim.pro', 'https://trasnferchat-1336.twil.io/transferChat', 'https://trasnferchat-1336.twil.io', 'https://ultim.pro/dashboard/carestream/chat-carestream'];
 
